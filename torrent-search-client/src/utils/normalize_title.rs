bc34485a8e49ac9d6a6ee10556a1c21fd14e73ba
Netfloex <38650595+Netfloex@users.noreply.github.com>
2023-12-20 01:42:50 +0100

    Improve parsing of torrent title: use Levenshtein

--- torrent-search-client/src/utils/normalize_title.rs
+++ torrent-search-client/src/utils/normalize_title.rs
@@ -2,13 +2,12 @@ use lazy_static::lazy_static;
 use regex::Regex;
 
 lazy_static! {
-    static ref SITE_REGEX: Regex = Regex::new(r"www\.\w+\.com").unwrap();
-    static ref NORMALIZE_BOUNDARIES: Regex = Regex::new(r"[\.:-]").unwrap();
+    static ref NORMALIZE_BOUNDARIES: Regex = Regex::new(r"[-._:]").unwrap();
     static ref MULTIPLE_SPACES: Regex = Regex::new(r"\s{2,}").unwrap();
 }
 
 pub fn normalize_title(title: &str) -> String {
-    let title = SITE_REGEX.replace_all(&title, "");
+    let title = title.to_lowercase();
     let title = NORMALIZE_BOUNDARIES.replace_all(&title, " ");
     let title = MULTIPLE_SPACES.replace_all(&title, " ");
     let title = title.replace(|c: char| !c.is_ascii(), "");
--- torrent-search-client/src/utils/parse_title.rs
+++ torrent-search-client/src/utils/parse_title.rs
@@ -1,57 +1,50 @@
-use std::ops::AddAssign;
-
+use distance::levenshtein;
 use lazy_static::lazy_static;
 use regex::Regex;
 
 use crate::utils::normalize_title;
 
 lazy_static! {
-    static ref REMOVE_AFTER_YEAR: Regex = Regex::new(r"[\s(.]*\d{4}.*").unwrap();
-    static ref REMOVE_TAGS_REGEX: Regex = Regex::new(r"[\[(].*[\])]").unwrap();
+    static ref REMOVE_AFTER_YEAR: Regex = Regex::new(r"\W*(19|20)\d\d\D.*").unwrap();
+    static ref REMOVE_TAGS_REGEX: Regex = Regex::new(r"\[.*\]|\(.*\)").unwrap();
     static ref YEAR_REGEX: Regex = Regex::new(r"19|20\d\d").unwrap();
+    static ref SITE_REGEX: Regex = Regex::new(r"(www\.)?\w+\.(com|me|to)").unwrap();
 }
 
 pub fn parse_title(title: &str) -> String {
+    let title = title.to_lowercase();
+
     let Some(year) = YEAR_REGEX.find(&title) else {
         return String::new();
     };
 
     let title = REMOVE_AFTER_YEAR.replace(&title, "");
     let title = REMOVE_TAGS_REGEX.replace(&title, "");
+    let title = SITE_REGEX.replace(&title, "");
     let title = normalize_title(&title);
+
     let title = title.trim();
 
     format!("{} ({})", title, year.as_str())
 }
 
-fn match_count(first: &str, second: &str) -> f32 {
-    let mut count = 0;
-    let max_count = usize::min(first.len(), second.len());
+fn levenshtein_percentage(first: &str, second: &str) -> f64 {
+    let distance = levenshtein(&first, &second);
 
-    let first = first.to_ascii_lowercase();
-    let mut first = first.chars();
+    let max_len = first.len().max(second.len());
 
-    let second = second.to_ascii_lowercase();
-    let mut second = second.chars();
+    let match_perc = 1.0 - (distance as f64 / max_len as f64);
 
-    for _ in 0..max_count {
-        if let Some(a) = first.next() {
-            if let Some(b) = second.next() {
-                if a.eq_ignore_ascii_case(&b) {
-                    count.add_assign(1);
-                }
-            }
-        }
-    }
-
-    (count as f32) / (max_count as f32)
+    match_perc
 }
 
-pub fn is_title_match(movie_title: &str, torrent_title: &str) -> bool {
-    let torrent_title = parse_title(torrent_title);
+pub fn is_title_match(movie_title: &str, og_torrent_title: &str) -> bool {
+    let torrent_title = parse_title(og_torrent_title);
+    // println!("{}\n{}\n", og_torrent_title, torrent_title);
 
     let movie_title = normalize_title(movie_title);
-    let match_perc = match_count(&torrent_title, &movie_title);
+
+    let match_perc = levenshtein_percentage(&movie_title, &torrent_title);
 
     let matches = match_perc > 0.8;
     if !matches {
