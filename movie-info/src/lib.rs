f5207236db974dbd7a9ca5ef170c7d96b45173b6
Netfloex <38650595+Netfloex@users.noreply.github.com>
2024-01-16 15:12:58 +0100

    Add caching to `movie-info`

--- movie-info/src/lib.rs
+++ movie-info/src/lib.rs
@@ -2,6 +2,7 @@ mod api;
 mod error;
 mod models;
 pub use error::Error;
+use http_cache_surf::{CACacheManager, Cache, CacheMode, HttpCache, HttpCacheOptions};
 pub use models::movie_info::MovieInfo;
 use surf::{Client, Config};
 use utils::surf_logging::SurfLogging;
@@ -15,7 +16,13 @@ impl MovieInfoClient {
         let client: Client = config.try_into().unwrap();
 
         Self {
-            http: client.with(SurfLogging),
+            http: client
+                .with(Cache(HttpCache {
+                    mode: CacheMode::ForceCache,
+                    manager: CACacheManager::default(),
+                    options: HttpCacheOptions::default(),
+                }))
+                .with(SurfLogging),
         }
     }
 }
