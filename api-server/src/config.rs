1a56758ea9da16307bf80f1ed495a77c948fd08b
Netfloex <38650595+Netfloex@users.noreply.github.com>
2024-01-18 20:54:55 +0100

    Use `serde_inline_default` for config

--- api-server/src/config.rs
+++ api-server/src/config.rs
@@ -5,56 +5,37 @@ use figment::{
     providers::{Env, Format, Yaml},
     Error,
 };
-
 use serde::{Deserialize, Serialize};
+use serde_inline_default::serde_inline_default;
 
-fn default_movies_path() -> PathBuf {
-    PathBuf::from("./movies")
-}
-
-fn default_category() -> String {
-    "torrent-api".to_string()
-}
-
-fn default_movie_tracking() -> bool {
-    false
-}
-
-fn default_movie_tracking_max_timeout_active() -> u64 {
-    60
-}
-
-fn default_movie_tracking_timeout_inactive() -> u64 {
-    300
-}
-
-fn default_movie_tracking_min_timeout() -> u64 {
-    1
-}
-
+#[serde_inline_default]
 #[derive(Debug, Serialize, Deserialize, Getters, Clone)]
 pub struct QbittorrentConf {
     username: String,
     password: String,
     url: String,
-    #[serde(default = "default_category")]
+    #[serde_inline_default("torrent-api".to_string())]
     category: String,
 }
 
+#[serde_inline_default]
 #[derive(Debug, Serialize, Deserialize, Getters, Clone)]
 pub struct Config {
     qbittorrent: QbittorrentConf,
     remote_download_path: String,
     local_download_path: String,
-    #[serde(default = "default_movies_path")]
     movies_path: PathBuf,
-    #[serde(default = "default_movie_tracking")]
+
+    #[serde_inline_default(false)]
     disable_movie_tracking: bool,
-    #[serde(default = "default_movie_tracking_max_timeout_active")]
+
+    #[serde_inline_default(60)]
     movie_tracking_max_timeout_active: u64,
-    #[serde(default = "default_movie_tracking_timeout_inactive")]
+
+    #[serde_inline_default(3600)]
     movie_tracking_timeout_inactive: u64,
-    #[serde(default = "default_movie_tracking_min_timeout")]
+
+    #[serde_inline_default(1)]
     movie_tracking_min_timeout: u64,
 }
 
